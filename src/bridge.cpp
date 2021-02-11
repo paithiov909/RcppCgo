d16e5322c8a9ca8b87cf143b2aa9d5f2bca34429
Kato Akiru <am11.44@gmail.com>
2021-02-11 11:09:17 +0900

    Add RcppThread dependency

--- src/bridge.cpp
+++ src/bridge.cpp
@@ -1,5 +1,8 @@
 // [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::depends(RcppThread)]]
+#define STRICT_R_HEADERS
 #define R_NO_REMAP
+#define RCPPTHREAD_OVERRIDE_THREAD 1
 #include <Rcpp.h>
 #include <cstdlib>
 #include "../inst/include/librcgo.h"
@@ -19,21 +22,24 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 Rcpp::CharacterVector hi(Rcpp::CharacterVector v) {
 
-  std::function< Rcpp::String(Rcpp::String) > func_obj = [](Rcpp::String x) {
+  char* response;
+
+  std::function< Rcpp::String(Rcpp::String) > func = [&](Rcpp::String x) {
     const char* s = x.get_cstring();
     const std::size_t n = std::strlen(s);
     const std::ptrdiff_t len = n;
     const GoString message = { s, len };
 
-    char* response = Hi(message);
+    response = Hi(message);
     const std::string res = response;
-
-    free(response);
 
     const Rcpp::String result = res;
     return result;
   };
 
-  const Rcpp::CharacterVector result = sapply(v, func_obj);
+  const Rcpp::CharacterVector result = sapply(v, func);
+
+  free(response);
+
   return result;
 }
